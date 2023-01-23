2e4ed60a7c9291ee374ace12907e31d9eb1d4599
Aditya Saxena <aditya@MacBook-Air-2.local>
2023-01-23 11:17:07 -0800

    update with new UI

--- productDCL/assets/js/main_bk.js
+++ productDCL/assets/js/main_bk.js
@@ -1,77 +1,39 @@
-window.CMUtils = window.CMUtils || {};
-
-CMUtils.enforceMaxLength = function (cm, change) {
-  var maxLength = cm.getOption("maxLength");
-  if (maxLength && change.update) {
-    var str = change.text.join("\n");
-    var delta = str.length - (cm.indexFromPos(change.to) - cm.indexFromPos(change.from));
-    if (delta <= 0) { return true; }
-    delta = cm.getValue().length + delta - maxLength;
-    if (delta > 0) {
-      str = str.substr(0, str.length - delta);
-      change.update(change.from, change.to, str.split("\n"));
-    }
-  }
-  return true;
-}
-
-// setting no.of characters left for prompts
-function setElemPrompt(prompt, val) {
-  var txtAreaVal = $(prompt).val()
-  $(val).html(txtAreaVal.length)
-}
-
-// creating codeMirror Instance
+// creating codeMirror Instance 
 let editorsArr = ['editor001'];
-let themeName = 'idea';
+let themeName = 'dracula';
 const MaxCharacter = 5000;
 
-var editorArray = []
+var editor1 = []
 var lu = 1;
-var tmplu = scrptlu = 1;
+var tmplu = scrptlu = 1 ;
+
 
 // initializing the code mirror
 var editor = CodeMirror.fromTextArea(document.getElementById('editor001'), {
-  mode: 'text/typescript',
-  theme: themeName,
+  mode: 'lua',
+  theme: 'dracula',
   lineNumbers: true,
-  autoRefresh: true,
-  matchBrackets: true,
   autoCloseTags: true,
   maxLenght: MaxCharacter,
-  autofocus: true,
 })
 editor.setSize('100%', '450')
 
-editor.setOption("maxLength", MaxCharacter);
-editor.on("beforeChange", CMUtils.enforceMaxLength);
+editor1 = [editor]
 
-editor.on('change', editor => {
-  let x = editor.getValue();
-  $('#editorMaxVal').html(x.length)
-})
 
-$('#editor001').data('CodeMirrorInstance', editor);
-var codemirrorEditor = $('#editor001').data('CodeMirrorInstance');
-
-editorArray = [editor]
-
-// limit the editor instances
-const maxEditors = 4;
-
-for (var i = 1; i < maxEditors; i++) {
-
+function createEditor() {
   var d = new Date();
   var time = d.getTime();
   var rand = Math.random() * 100;
+
   rand = Math.floor(rand + time);
+
 
   let editorhtml = `
     <div class="tab-pane TabEditor fade" id="tab${rand}" value="${lu}">
-    <textarea id="editor${rand}">/** This is editor ${++scrptlu} */
-var global_var = 10
-</textarea>
-      <input type="hidden" value="${lu}" class="inp">
+    <textarea id="editor${rand}">print("Script ${++scrptlu}")</textarea>
+    <input type="hidden" value="${lu}" class="inp">
+
     </div>
   `;
   let tabhtml = `
@@ -84,80 +46,71 @@ var global_var = 10
 
   // initializing the code mirror
   editor = CodeMirror.fromTextArea(document.getElementById(`editor${rand}`), {
-    mode: 'text/typescript',
+    mode: 'lua',
     theme: themeName,
     lineNumbers: true,
     autoRefresh: true,
-    matchBrackets: true,
     autoCloseTags: true,
     maxLenght: MaxCharacter,
     autofocus: true,
   })
 
-  editor.setSize('100%', '450')
-
-  editor.on('change', editor => {
-    let x = editor.getValue();
-    $('#editorMaxVal').html(x.length)
-  })
-
-  lu++
+lu++
 
   $(`.CodeMirror-sizer`).addClass('customMirror');
   $(`#editor${rand}`).data('CodeMirrorInstance', editor);
-  editor.setOption("maxLength", MaxCharacter);
-  editor.on("beforeChange", CMUtils.enforceMaxLength);
 
-  editorArray.push(editor)
-  // codemirrorEditor = editor
+  editor1.push(editor)
 
-  let x = editor.getValue();
-  $('#editorMaxVal').html(x.length)
 }
 
 
-/* opening the specific tab based on index*/
-let defaultIndex = 1;
-$.each($('.openTab'), function (index, val) {
-  $(this).removeClass('active')
-})
-$.each($('.TabEditor'), function (index, val) {
-  $(this).removeClass('active')
-  $(this).addClass('fade')
-})
-let ttb = document.getElementsByClassName('TabEditor')[defaultIndex];
-ttb.classList.add('active');
-ttb.classList.remove('fade');
-document.getElementsByClassName('openTab')[defaultIndex].classList.add('active');
-codemirrorEditor = editorArray[defaultIndex]
-
-
 /* max-length */
+window.CMUtils = window.CMUtils || {};
+
+CMUtils.enforceMaxLength = function(cm, change) {
+  var maxLength = cm.getOption("maxLength");
+  if (maxLength && change.update) {
+      var str = change.text.join("\n");
+      var delta = str.length-(cm.indexFromPos(change.to) - cm.indexFromPos(change.from));
+      if (delta <= 0) { return true; }
+      delta = cm.getValue().length+delta-maxLength;
+      if (delta > 0) {
+          str = str.substr(0, str.length-delta);
+          change.update(change.from, change.to, str.split("\n"));
+      }
+  }
+  return true;
+}
+
+editor.setOption("maxLength", MaxCharacter);
+editor.on("beforeChange", CMUtils.enforceMaxLength);
+
 function checkTextCharacters(ch) {
   var x = '';
-  for (var i = 0; i < ch.length; i++) {
-    if (i < MaxCharacter) {
+  for(var i = 0; i < ch.length; i++) {
+    if(i < MaxCharacter) {
       x += ch[i];
     }
   }
   return x;
 }
-/* setting editor max lenght etc */
 
-function countTextCharacters(ed) {
-  var x = editor.getValue()
+function countTextCharacters() {
+  var x = editor.getValue()  
   return x.length;
 }
-function getSetNums(ed) {
-  var x = countTextCharacters(ed);
-  $('#editorMaxVal').html(x)
-}
-getSetNums(editor);
 
-editor.on('keyup', () => {
-  getSetNums(editor)
+editor.on('keyup', ()=> {
+  var x = countTextCharacters();
+  $('#editorMaxVal').html(x)
 })
+
+var x = countTextCharacters();
+$('#editorMaxVal').html(x)
+
 /* checking max-length ends here */
+
 
 // Functions of Editor
 $(document).ready(function () {
@@ -166,33 +119,45 @@ $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip()
   })
 
+  var codeInstance = '#editor001';
   // var codemirrorEditor = $('#editor001').nextAll('.CodeMirror')[0].CodeMirror
+  
+  $('#editor001').data('CodeMirrorInstance', editor);
+  var codemirrorEditor = $('#editor001').data('CodeMirrorInstance');
 
-  $(document).on('click', '.openTab', function () {
+  $(document).on('click', '.openTab', function() {
+    // console.log(editor1)
+    codeMrrInstance = parseInt($('#appendeditor .tab-pane.active').attr('value'));
 
     setTimeout(() => {
+      
       codeMrrInstance = $('#appendeditor .tab-pane.active .inp').val();
+      console.log(codeMrrInstance)
+      codemirrorEditor = editor1[codeMrrInstance]
+    }, 500);
 
-      codemirrorEditor = editorArray[codeMrrInstance]
-
-      let y = editorArray[codeMrrInstance].getValue()
-      $('#editorMaxVal').html(y.length)
-    }, 400);
-  })
-
-  codemirrorEditor.on('keyup', () => {
-    let x = codemirrorEditor.getValue()
-    $('#editorMaxVal').html(x.length)
+    console.log()
   })
 
   // changing the theme
   $('#changeTheme').on('change', function () {
     themeName = $(this).val()
-    for (i in editorArray) {
-      editorArray[i].setOption('theme', themeName)
+    for(i in editor1) {
+      console.log(i)
+      editor1[i].setOption('theme', themeName)
     }
   })
   $('#changeTheme').select2()
+
+  // textarea lenght counting
+  $('#elemprompt').on('keyup', () => {
+    var txtAreaVal = $(this).val()
+    txtAreaVal = document.getElementById('elemprompt').value
+    $('#txtlenght').html(txtAreaVal.length)
+  })
+
+  // clearing the codemirror code
+  // var codemirrorEditor = $('#editor001').nextAll('.CodeMirror')[0].CodeMirror
 
   $('#hide').click(function () {
     codemirrorEditor.getDoc().setValue('')
@@ -235,6 +200,7 @@ $(document).ready(function () {
             a.click()
             document.body.removeChild(a)
           }
+
           break
       }
     })
@@ -260,4 +226,54 @@ $(document).ready(function () {
     })
   })
 
+  // setting the value of editor
+  let Ex1 = `other = require "other"
+other.hi()
+    
+for i = 1, #arg do
+    local str = string.format("Arg %i is %q", i, arg[i])
+    print(str)
+end`
+
+  let Ex2 = `--[[
+this is Example 2
+]]`
+
+  let Ex3 = `print("Hello World")`
+
+  $('#Ex1').on('click', function () {
+  
+    
+    Ex1 = checkTextCharacters(Ex1);
+    codemirrorEditor.setValue(Ex1)
+
+    // counting the value of characters
+    var x = countTextCharacters();
+    $('#editorMaxVal').html(x)
+  })
+
+  $('#Ex2').on('click', function () {
+    Ex2 = checkTextCharacters(Ex2);
+    codemirrorEditor.setValue(Ex2)
+
+    // counting the value of characters
+    var x = countTextCharacters();
+    $('#editorMaxVal').html(x)
+  })
+
+  $('#Ex3').on('click', function () {
+    Ex3 = checkTextCharacters(Ex3);
+    codemirrorEditor.setValue(Ex3)
+
+    // counting the value of characters
+    var x = countTextCharacters();
+    $('#editorMaxVal').html(x)
+  })
+
+  // getting the value from the textarea and setting in the editor
+  $('#generateCode').on('click', function () {
+    // getting the value
+    let textval = $('#elemprompt').val()
+    codemirrorEditor.setValue(textval)
+  })
 })
